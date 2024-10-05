b749d50c00af9d0df6fce3ac5162b6f35877c833
Michael Levus <misha.levus@gmail.com>
2024-10-05 17:07:53 +0300

    lecture#1 Finish task#1

---
+++ lecture01/index.js
@@ -0,0 +1,18 @@
+/*
+Task: Write a program that outputs numbers from 1 to 10
+using a for loop and a while loop.
+*/
+
+// Output numbers from 1 to 10 using a for loop
+console.log('For loop:');
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Output numbers from 1 to 10 using a while loop
+console.log('While loop:');
+let j = 1;
+while (j <= 10) {
+  console.log(j);
+  j++;
+}
